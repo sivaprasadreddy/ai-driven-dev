715756384da6e91ff5e67dd722688a79acb2f3fd
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2025-08-11 07:44:07 +0530

    feat: init spring boot project

---
+++ src/test/java/com/sivalabs/urlshortener/TestcontainersConfiguration.java
@@ -0,0 +1,18 @@
+package com.sivalabs.urlshortener;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MariaDBContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+    @Bean
+    @ServiceConnection
+    MariaDBContainer<?> mariaDbContainer() {
+        return new MariaDBContainer<>(DockerImageName.parse("mariadb:11.8.2"));
+    }
+
+}
